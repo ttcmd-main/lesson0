bcabe6e1563e98d7c96db75b3e66d85a85ff86e6
ttcmd-main <118116436+ttcmd-main@users.noreply.github.com>
2024-08-29 23:41:01 +0300

    Add files via upload

---
+++ lesson0/main.py
@@ -0,0 +1,16 @@
+#1st program - task1
+print(9**0.5*5)
+
+#2nd program - task2
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd program - task3
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#4th program
+print (int(float('123.456') * 10) % 10)
+
+
+
